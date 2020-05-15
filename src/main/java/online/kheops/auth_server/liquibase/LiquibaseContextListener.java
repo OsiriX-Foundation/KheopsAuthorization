86164f2df10b8df17c52d6869cc21b3ebcafa997
Joël Spaltenstein <spalte@naturalimage.ch>
2020-05-15 11:40:22 +0200

    liquibase connection

--- src/main/java/online/kheops/auth_server/liquibase/LiquibaseContextListener.java
+++ src/main/java/online/kheops/auth_server/liquibase/LiquibaseContextListener.java
@@ -5,12 +5,13 @@ import liquibase.database.Database;
 import liquibase.database.DatabaseFactory;
 import liquibase.database.jvm.JdbcConnection;
 import liquibase.resource.ClassLoaderResourceAccessor;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import online.kheops.auth_server.util.JOOQTools;
+//import org.hibernate.boot.MetadataSources;
+//import org.hibernate.boot.registry.StandardServiceRegistry;
+//import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
-import org.hibernate.engine.jdbc.connections.spi.ConnectionProvider;
+//import org.hibernate.engine.jdbc.connections.spi.ConnectionProvider;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
@@ -47,14 +48,14 @@ public class LiquibaseContextListener implements ServletContextListener {
         final Configuration cfg = new Configuration();
         cfg.setProperties(properties);
 
-            try {
+            try (Connection con = JOOQTools.getDataSource().getConnection()){
 
-                // Prepare the Hibernate configuration
-                StandardServiceRegistry reg = new StandardServiceRegistryBuilder().applySettings(cfg.getProperties()).build();
-                MetadataSources metaDataSrc = new MetadataSources(reg);
-
-                // Get database connection
-                Connection con = metaDataSrc.getServiceRegistry().getService(ConnectionProvider.class).getConnection();
+//                // Prepare the Hibernate configuration
+//                StandardServiceRegistry reg = new StandardServiceRegistryBuilder().applySettings(cfg.getProperties()).build();
+//                MetadataSources metaDataSrc = new MetadataSources(reg);
+//
+//                // Get database connection
+//                Connection con = metaDataSrc.getServiceRegistry().getService(ConnectionProvider.class).getConnection();
                 JdbcConnection jdbcCon = new JdbcConnection(con);
 
                 // Initialize Liquibase and run the update
@@ -70,7 +71,7 @@ public class LiquibaseContextListener implements ServletContextListener {
                 }
                 liquibase.validate();
                 jdbcCon.close();
-                con.close();
+//                con.close();
             } catch (Exception e) {
                 LOG.log(Level.SEVERE, "Unable to use liquibase", e);
                 exit(1);
